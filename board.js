8c1379a90590adb36f03eb0ce742291bb37a798d
Endo_Hizumi <endohizumi0114@gmail.com>
2020-03-27 18:17:36 +0900

    update: when in stock sticky clicked, append sticky in canvas.

--- board.js
+++ board.js
@@ -1,10 +1,18 @@
 window.onload = function () {
-  function onClick(e) {
-    var div = document.createElement('textarea');
-    div.className = 'sticky in_stock';
-    document.getElementById('stock').appendChild(div);
+  function Append_onClick(e) {
+    var sticky = document.createElement('textarea');
+    sticky.className = 'sticky in_stock';
+    document.getElementById('stock').appendChild(sticky);
+    sticky.addEventListener('click', in_stock_onClick, false);
+  }
+
+  function in_stock_onClick(e) {
+    var sticky = document.createElement('textarea');
+    sticky.className = 'sticky in_canvas';
+    document.getElementById('canvas').appendChild(sticky);
   }
 
   append = document.getElementById('Append')
-  append.addEventListener('click', onClick, false);
+  append.addEventListener('click', Append_onClick, false);
+
 }

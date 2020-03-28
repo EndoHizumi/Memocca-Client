9293529417aa3b8c77d8bb118ba1f95ba6a9fbd1
Endo_Hizumi <endohizumi0114@gmail.com>
2020-03-29 01:02:22 +0900

    draggable a sticky

--- board.js
+++ board.js
@@ -1,18 +1,34 @@
 window.onload = function () {
-  function Append_onClick(e) {
-    var sticky = document.createElement('textarea');
-    sticky.className = 'sticky in_stock';
-    document.getElementById('stock').appendChild(sticky);
-    sticky.addEventListener('click', in_stock_onClick, false);
-  }
-
-  function in_stock_onClick(e) {
+  
+  function inStockOnClick(e) {
     var sticky = document.createElement('textarea');
     sticky.className = 'sticky in_canvas';
+    sticky.style.backgroundColor = e.target.style.backgroundColor;
+    sticky.addEventListener("mousedown",function() {
+      sticky.addEventListener("mousemove",onMouseMove)
+    })
+    sticky.addEventListener("mouseup",function() {
+      sticky.removeEventListener("mousemove",onMouseMove)
+    })
+    sticky.addEventListener("mouseleave",function(e){
+      console.log(e.target.value)
+      e.target.blur()
+    })
     document.getElementById('canvas').appendChild(sticky);
   }
 
-  append = document.getElementById('Append')
-  append.addEventListener('click', Append_onClick, false);
+  function onMouseMove(e){
+      var x = e.clientX;
+      var y = e.clientY;
+      var width = e.target.offsetWidth;
+      var height = e.target.offsetHeight;
+      console.log(e.target.style.cursor)
+      e.target.style.top = y + "px";
+      e.target.style.left = x + "px";
+  }
 
+  in_stock_stickies =  Array.from(document.getElementsByClassName('in_stock'))
+  in_stock_stickies.forEach(sticky => {
+    sticky.addEventListener('click', inStockOnClick, false);
+  });
 }

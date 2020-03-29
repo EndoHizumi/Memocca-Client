fdf43240e1c674602dc8aef7fee6a8da3b80fc33
Endo_Hizumi <endohizumi0114@gmail.com>
2020-03-29 22:09:40 +0900

    update: many update

--- board.js
+++ board.js
@@ -1,22 +1,36 @@
 window.onload = function () {
-  
+
+  isChanged = false
   function inStockOnClick(e) {
-    var sticky = document.createElement('textarea');
+    sticky = createSticky(e)
+    sticky.style
+    document.getElementById('canvas').appendChild(sticky);
+  }
+
+  function createSticky(event){
+    sticky = document.createElement('div');
     sticky.className = 'sticky in_canvas';
+    sticky.backgroundColor = "black"
+    sticky.appendChild(createTextArea(event));
     $(sticky).draggable();
+    return sticky
   }
 
-  function onMouseMove(e){
-      var x = e.clientX;
-      var y = e.clientY;
-      var width = e.target.offsetWidth;
-      var height = e.target.offsetHeight;
-      console.log(e.target.style.cursor)
-      e.target.style.top = y + "px";
-      e.target.style.left = x + "px";
+  function createTextArea(e) {
+    textarea = document.createElement('textarea');
+    textarea.style.backgroundColor = e.target.style.backgroundColor;
+    textarea.addEventListener("mouseleave", function (e) {
+      if(isChanged){
+        console.log(e.target.value);
+        isChanged = false
+      }
+      e.target.blur();
+    })
+    textarea.addEventListener("input", function() {isChanged=true})
+    return textarea
   }
 
-  in_stock_stickies =  Array.from(document.getElementsByClassName('in_stock'))
+  in_stock_stickies = Array.from(document.getElementsByClassName('in_stock'))
   in_stock_stickies.forEach(sticky => {
     sticky.addEventListener('click', inStockOnClick, false);
   });

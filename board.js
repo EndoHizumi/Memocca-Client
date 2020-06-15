8b2ad0ab45d961e6ac725633bd9634d3ea200a39
Endo_Hizumi <endohizumi0114@gmail.com>
2020-06-16 02:02:46 +0900

    wip: source share

--- board.js
+++ board.js
@@ -1,6 +1,6 @@
 window.onload = function () {
-// 　const api_domain='api.ws-factory.app/memocca'
-  const api_domain='http://localhost:3000'
+  // 　const api_domain='api.ws-factory.app/memocca'
+  const api_domain = 'http://localhost:3000'
   //やっぱり、ログイン画面で部屋と名前を入力してからボードに入る方が楽
   // index.htmlで部屋入力→/authを叩いて、部屋IDを確認→あったら、部屋IDを返す（なかったら、401エラー）
   // 返ってきたIDをboard.htmlのクエリに追加して、リクエスト(ex: board.html?boardId=hogehoge)
@@ -8,58 +8,62 @@ window.onload = function () {
   // board_idをクエリから取得
   // 付箋取得APIを叩いて、付箋を取得
   // 付箋を描画
-  const board_id = (new URL(document.location)).searchParams.get('boardId')
-  sticky = fetch(`${api_domain}/sticky/${board_id}`).then(response => response.ok ? response.json: "" )
-
   isChanged = false
-  function inStockOnClick(e) {
-    sticky = createSticky(e)
-    document.getElementById('canvas').appendChild(sticky);
-  }
+  const board_id = (new URL(document.location)).searchParams.get('boardId')
+  stickies = fetch(`${api_domain}/sticky/${board_id}`).then(response => { return response.ok ? response.json() : "" }).then(stickies => this.console.log(stickies))
+  // sticky_text = e.target.text
 
-  function drawSticky(color, text, x, y) {
+  function drawSticky(color, text, x, y, width, height) {
     // サーバーからもらったデータをもとに付箋を追加できるように、する
     sticky = document.createElement('div');
     sticky.className = 'sticky in_canvas';
     sticky.style.position = "absolute"
     sticky.style.top = x;
     sticky.style.left = y;
+    sticky.style.width = width;
+    sticky.style.height = height;
     sticky.appendChild(createButton())
     sticky.appendChild(createTextArea(color, text));
     $(sticky).draggable();
   }
 
-  function createSticky(event) {
+  function createSticky(color, text) {
     // サーバーからもらったデータをもとに付箋を追加できるように、する
-    drawSticky(event)
+    drawSticky(color, text, 0, 0, 300, 150)
     console.log("[POST] ./sticky/{board_id}")
     return sticky
   }
 
-  function createButton(params) {
+  function createButton() {
     button = document.createElement('button')
     button.innerHTML = 'X'
-    button.addEventListener('click',function(e) {
+    button.addEventListener('click', function (e) {
       document.getElementById('canvas').removeChild(e.target.parentElement)
       console.log("[DELETE] ./sticky/{board_id}")
     })
     return button
   }
 
-  function createTextArea(e) {
+  function createTextArea(rgb, text) {
     textarea = document.createElement('textarea');
-    textarea.style.backgroundColor = e.target.style.backgroundColor;
-    textarea.value = "";
+    textarea.style.backgroundColor = rgb;
+    textarea.value = text;
     textarea.addEventListener("mouseleave", function (e) {
-      if(isChanged){
+      if (isChanged) {
         console.log("[PUT] ./sticky/{board_id}")
         console.log(e.target.value);
         isChanged = false
       }
-      e.target.blur();
+      this.blur();
     })
-    textarea.addEventListener("input", function() {isChanged=true})
+    textarea.addEventListener("input", function () { isChanged = true })
     return textarea
+  }
+
+  function inStockOnClick(e) {
+    sticky_color = e.target.style.backgroundColor
+    sticky = createSticky(sticky_color, "")
+    document.getElementById('canvas').appendChild(sticky);
   }
 
   this.console.log("[GET] ./sticky/{board_id}")
